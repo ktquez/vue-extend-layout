3d6fed33bd56b101ef729cf0bb3c91457fff1803
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2017-09-16 12:47:59 -0300

    Remove param deprecated dirname

--- src/layouts.js
+++ src/layouts.js
@@ -1,10 +1,9 @@
 /**
  * Retrieve context components to global register on Vue
  * @export
- * @param {String} dirname
  * @return {Array} With layout components
  */
-export default function layouts (dirname) {
+export default function layouts () {
   const layouts = require.context('@/layouts', false, /^\.\/.*\.vue$/)
   return layouts.keys().map(layouts)
 }
