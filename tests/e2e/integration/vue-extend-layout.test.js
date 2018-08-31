fcba4fb13899009b06405c38ec43d53f1868df61
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2018-08-31 10:37:51 -0300

    Add integration test by Cypress

--- tests/e2e/integration/vue-extend-layout.test.js
+++ tests/e2e/integration/vue-extend-layout.test.js
@@ -3,30 +3,24 @@ describe('Vue Layout Test', () => {
     cy.visit('/')
   })
 
-  it('Check if you\'ve loaded the page header that is in the default layout', () => {
-    cy.get('h1[data-vel="header"]').contains('Welcome to Your Vue.js App')
+  it('Must have default layout title and view subtitle', () => {
+    cy.get('h1[data-vel="header"]').contains('This layout is DEFAULT')
+    cy.get('h2[data-vel="subheader"]').contains('Welcome to Your Vue.js App')
   })
 
-  it('Header should not exist on page about', () => {
+  it('Must have page layout title and view subtitle', () => {
     cy.get('a[href="/about"]').click()
-    cy.get('h1[data-vel="header"]').should('not.exist')
+    cy.get('h1[data-vel="header"]').contains('This layout is PAGE')
+    cy.get('h2[data-vel="subheader"]').contains('This is an about page')
   })
 })
 
-describe('Vue Layout Test with delay', () => {
+describe('Vue Layout with delay', () => {
   beforeEach(() => {
     cy.visit('/contact')
   })
 
-  it('Should not load default layout on route with delay', () => {
-    cy.get('h1[data-vel="header"]').should('not.exist')
-  })
-
-  it('Must load loading layout after 2 seconds delay', () => {
-    cy.get('h1[data-vel="header-loader"]', {timeout: 2000}).contains('Loading...')
-  })
-
   it('Must show the title of the contact page after 6 seconds of delay', () => {
-    cy.get('h2[data-vel="header-contact"]', {timeout: 6000}).contains('Contact Page')
+    cy.get('h2[data-vel="subheader"]', {timeout: 6000}).contains('This is an contact page')
   })
 })
