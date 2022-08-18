5a2960aa6fb49d703184d8ed06a0545108af325f
donTsipa <don_tsipa@mail.ru>
2022-08-18 15:59:18 +0300

    remove example

--- example/src/components/Navigation.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <nav id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link :to="{ path: '/', query: { plan: 'content'} }">Home with query param</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/contact">Contact</router-link>
-  </nav>
-</template>
--- example/src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h2 data-vel="subheader">This is an about page</h2>
-  </div>
-</template>
